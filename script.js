9af755e82d7df9f205d14c9f0029c7ab156ebbf3
Codeweb04 <prashinmore@gmail.com>
2021-01-25 16:36:32 +0530

    commit7

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 toggleBtn = document.querySelector("#toggle-btn") 
 menuList = document.querySelector("#menu-list")
+container = document.getElementsByClassName('container-div')[0]
 
 toggleBtn.addEventListener('click', () => {
     menuList.classList.toggle("hide-menu")
+    container.classList.toggle("marginleft")
+    console.log(container)
 })
 
